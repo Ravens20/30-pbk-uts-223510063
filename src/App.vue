6844ab1dc377114c186d02adb0caa8b5b2c00153
Ravens20 <dindradesvantiod3@gmail.com>
2024-04-29 20:49:46 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!-- App.vue -->
+<template>
+  <div>
+    <RegistrationForm />
+  </div>
+</template>
+
+<script>
+import RegistrationForm from './components/RegistrationForm.vue';
+
+export default {
+  components: {
+    RegistrationForm
+  }
+};
+</script>
+
+<style>
+/* Styling sesuai kebutuhan */
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+// main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+import './styles.css'
+
+const app = createApp(App);
+
+app.config.globalProperties.$successMessage = function (message) {
+  alert(message);
+};
+
+app.mount('#app');
